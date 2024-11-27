56f9088bb24daed4d9d4e30e578962d2236f0a4e
mohammad126529 <mohammadabualrob794@gmail.com>
2024-11-27 07:56:44 +0300

    Models and Database Creation

---
+++ TequliasRestaurant/Models/Ingredient.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace TequliasRestaurant.Models
+{
+    public class Ingredient
+    {
+        public int IngredientId { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<ProductIngredient> ProductIngredients { get; set; }
+    }
+}
---
+++ TequliasRestaurant/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TequliasRestaurant.Models
+{
+    public class Product
+    {
+        public Product()
+        {
+            ProductIngredients = new List<ProductIngredient>();
+        }
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+
+     
+
+        public Category? Category { get; set; } //A product belongs to a category
+
+        public ICollection<OrderItem>? OrderItems { get; set; } //A product can be in many order items
+
+        public ICollection<ProductIngredient>? ProductIngredients { get; set; } //A product can have many ingredients
+
+    }
+}
